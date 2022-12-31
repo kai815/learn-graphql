3173f632f4e16a7afd8e4dc687212fe3952ab759
kai815 <h.kaizuka815@gmail.com>
2022-12-31 17:49:32 +0900

    :necktie: 1対多の接続

--- index.js
+++ index.js
@@ -8,12 +8,19 @@ const typeDefs = `
     LANDSCAPE
     GRAPHIC
   }
+  type User {
+  githubLogin: ID!
+  name: String
+  avatar: String
+  postedPhotos: [Photo!]!
+  }
   type Photo {
     id: ID!
     url: String!
     name: String!
     description: String
     category: PhotoCategory!
+    postedBy: User!
   }
   type Query {
     totalPhotos: Int!
@@ -30,8 +37,37 @@ const typeDefs = `
 `
 
 // 1. ユニークIDをインクリメントするための変数
-var _id = 0
-var photos = []
+let _id = 0
+
+// サンプル
+let users = [
+  { "githubLogin": "mHattrup", "name": "Mike Hattrup" },
+  { "githubLogin": "gPlake", "name": "Glen Plake" },
+  { "githubLogin": "sSchmidt", "name": "Scot Schmidt" }
+]
+
+let photos = [
+  {
+    "id": "1",
+    "name": "Dropping the Heart Chute",
+    "description": "The heart chute is one of my favorite chutes",
+    "category": "ACTION",
+    "githubUser": "gPlake"
+  },
+  {
+    "id": "2",
+    "name": "Enjoying the sunshine",
+    "category": "SELFIE",
+    "githubUser": "sSchmidt"
+  },
+  {
+    "id": "3",
+    "name": "Gunbarrel 25",
+    "description": "25 laps on gunbarrel today",
+    "category": "LANDSCAPE",
+    "githubUser": "sSchmidt"
+  }
+]
 
 const resolvers = {
   Query: {
@@ -42,7 +78,7 @@ const resolvers = {
   Mutation: {
     postPhoto(parent, args) {
       // 2.新しい写真を作成し、idを生成する
-      var newPhoto = {
+      const newPhoto = {
         id: _id++,
         ...args.input
       }
@@ -51,7 +87,15 @@ const resolvers = {
     }
   },
   Photo: {
-    url: parent => `http://yoursite.com/img/${parent.id}.jpg`
+    url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
+    postedBy: parent => {
+      return users.find(u => u.githubLogin === parent.githubUser)
+    }
+  },
+  User: {
+    postedPhotos: parent => {
+      return photos.filter(p => p.githubUser === parent.githubLogin)
+    }
   }
 }
 
