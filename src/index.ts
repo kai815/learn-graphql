4192e903c2fcdc5408de828a9fe3fd0cd8c01465
kai815 <h.kaizuka815@gmail.com>
2023-01-02 23:07:04 +0900

    mongodbへの接続

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,23 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 //TODO .jsなくてもよくしたい
 import { typeDefs } from './schema.js';
 import { resolvers } from './resolver.js';
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import * as dotenv from 'dotenv'
+dotenv.config()
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.PASS}@learngraphql.doxh4xt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+try {
+  const database = client.db('sample_mflix');
+  const movies = database.collection('movies');
+  // Query for a movie that has the title 'Back to the Future'
+  const query = { title: 'Back to the Future' };
+  const movie = await movies.findOne(query);
+  console.log(movie);
+} finally {
+  // Ensures that the client will close when you finish/error
+  await client.close();
+}
 
 
 // サーバーのインスタンスを作成
